1558e2bb2bc4b4e57f408e4b15824e828c39115e
Ben Nuttall <ben@bennuttall.com>
2012-11-30 23:47:41 +0000

    Third test failing

--- tests.py
+++ tests.py
@@ -12,5 +12,11 @@ class Test(unittest.TestCase):
         game.point_scored(1)
         self.assertEqual(game.get_score(), '15-0')
 
+    def test_player_one_scores_two_points_score_is_30_0(self):
+        game = Game()
+        game.point_scored(1)
+        game.point_scored(1)
+        self.assertEqual(game.get_score(), '30-0')
+
 if __name__ == '__main__':
     unittest.main()
