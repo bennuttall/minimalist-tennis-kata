b33e0300cde524147efe574b52462a9a08df0e70
Ben Nuttall <ben@bennuttall.com>
2012-11-30 23:46:25 +0000

    Refactored get_score return string to take tuple rather than each value of tuple

--- tennis.py
+++ tennis.py
@@ -3,7 +3,7 @@ class Game:
         self.score = (0, 0)
 
     def get_score(self):
-        return '%s-%s' % (self.score[0], self.score[1])
+        return '%s-%s' % (self.score)
 
     def point_scored(self, player):
         self.score = (15, 0)
