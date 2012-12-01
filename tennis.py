00b3cc8f83f77dea3ec6d2ded7e3b776b0ef4320
Ben Nuttall <ben@bennuttall.com>
2012-12-01 00:24:02 +0000

    Fifth test passing

--- tennis.py
+++ tennis.py
@@ -3,13 +3,16 @@ class Game:
         (0, 0): (15, 0),
         (15, 0): (30, 0),
         (30, 0): (40, 0),
+        (40, 0): ('Win'),
         }
 
     def __init__(self):
         self.score = (0, 0)
 
     def get_score(self):
-        return '%s-%s' % self.score
+        if type(self.score) is tuple:
+            return '%s-%s' % self.score
+        return 'Player 1 Wins'
 
     def point_scored(self, player):
         self.score = Game.point_scores[self.score]
