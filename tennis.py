3b911805478582b044957ad6bfb682688ec0e988
Ben Nuttall <ben@bennuttall.com>
2012-12-01 00:09:36 +0000

    Third test passing, now general rule for score transition after scoring a point

--- tennis.py
+++ tennis.py
@@ -1,9 +1,14 @@
 class Game:
+    point_scores = {
+        (0, 0): (15, 0),
+        (15, 0): (30, 0),
+        }
+
     def __init__(self):
         self.score = (0, 0)
 
     def get_score(self):
-        return '%s-%s' % (self.score)
+        return '%s-%s' % self.score
 
     def point_scored(self, player):
-        self.score = (15, 0)
+        self.score = Game.point_scores[self.score]
